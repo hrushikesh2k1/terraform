320a20e0af6f9987b2b4b02af28e00e0b7b0a128
Hrushikesh Boora <hrushikeshboora@gmail.com>
2025-08-31 20:12:23 +0530

    Update main.tf

--- Lab1/main.tf
+++ Lab1/main.tf
@@ -1,3 +1,5 @@
+# creating resource with custom names and with primary/secondary location.
+
 resource "azurerm_resource_group" "main" {
   name = "rg-${var.application_name}-${var.environment_name}"
   location = "${var.location}"
