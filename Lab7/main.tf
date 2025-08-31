ef4a033f189e8b3a46c649c4b233c44de5b22f0e
Hrushikesh Boora <hrushikeshboora@gmail.com>
2025-08-31 21:57:40 +0530

    Update main.tf

--- Lab7/main.tf
+++ Lab7/main.tf
@@ -4,7 +4,22 @@
 # role_definition_name = type of permission that is to be granted
 # scope = to which resources (it may be Individual resource, a resource group, a subscription or a management group)
 
-resource
 
-resource "azurerm_role_assignment" "example" {
-  scope = data.azure
+data "azurerm_client_config" "current" {
+}
+
+resource "azurerm_key_valut" "main" {
+  name = ""
+  location = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  tenant_id = data.azurerm_client_config.current.tenant_id
+  sku_name = "standard"
+}
+
+# you are assigning yourself with Key Vault Admin role
+resource "azurerm_role_assignment" "terraform_user" {
+  scope = azurerm_key_vault.main.id
+  role_definition_name = "Key Vault Administrator"
+  principal_id = data.azurerm_client_config.current.object_id
+}
+
