7d691c7ed43f5b11e9c623cbcd825d91ec0da189
Hrushikesh Boora <hrushikeshboora@gmail.com>
2025-08-31 20:58:41 +0530

    Create .debug-dev.sh

---
+++ Lab4/.debug-dev.sh
@@ -0,0 +1,15 @@
+# set the dev subscription
+export ARM_SUBSCRIPTION_ID = ""
+
+# set the application / environment
+export TF_VAR_application_name = "app1"
+export TF_VAR_environment_name = "dev"
+
+# run terraform 
+terraform init \ 
+    -backend-config="resource_group_name=rg-terraform-statefiles"
+    -backend-config="storage_account_name=stabcd123"
+    -backend-config="container_name=ctabcd123"
+    -backend-config="key=app1_dev"
+
+terraform $*
