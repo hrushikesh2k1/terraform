4ff2ff6c894a5fa7c98ef597a30a376b10c79952
Hrushikesh Boora <hrushikeshboora@gmail.com>
2025-08-31 21:24:57 +0530

    Update main.tf

--- Lab5/main.tf
+++ Lab5/main.tf
@@ -2,3 +2,17 @@ resource "azurerm_resource_group" "main" {
   name = "rg-${var.application_name}-${var.environment_name}"
   location = var.primary_location
 }
+
+resource "random_string" "keyvault_suffix" {
+  length = 6
+  upper = false
+  special = false
+}
+
+resource "azurerm_key_vault" "main" {
+  name = "kv-${var.application_name}-${var.environment_name}-${random_string.keyvault_suffix.result}"   # name should be globally unique
+  location = azurerm_resource_group.main.location
+  resource_group_name = azurerm_resource_group.main.name
+  tenant_id = ""
+  sku_name = "standard"
+}
