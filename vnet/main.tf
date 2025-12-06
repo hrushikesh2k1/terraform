481f550866e73bb333e92066b456227d7307662f
Hrushikesh Boora <hrushikeshboora@gmail.com>
2025-12-06 23:41:18 +0530

    Create main.tf

---
+++ vnet/main.tf
@@ -0,0 +1,27 @@
+# vnet, subnet, nic
+
+resource "azurerm_virtual_network" "myvnet" {
+  name                = "example-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = var.resource_group_name
+}
+
+resource "azurerm_subnet" "sn" {
+  name                 = "internal"
+  resource_group_name  = var.resource_group_name
+  virtual_network_name = azurerm_virtual_network.myvnet.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "mynic" {
+  name                = "example-nic"
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.sn.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
